4bef94c4e2795d35433e589f4bf0bc38bd827c95
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-12-20 23:08:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import os
 import telethon
 import asyncio
-from datetime import datetime, timezone, timedelta
 from telethon.errors import common
 from telethon.sync import TelegramClient
+import string
+import random
+
+
+bshwusbzws = "ANONYMOUS"
+i28xvbaaj = list(bshwusbzws)
+shuffle_count = 0
+
+def get_name_bwwu():
+  random.shuffle(i28xvbaaj)
+  global shuffle_count
+  shuffle_count += 1
+  if shuffle_count == 5:
+    shuffle_count = 0
+    b2is8wbs = bshwusbzws
+    return b2is8wbs
+  else:
+   b2is8wbs = "".join(i28xvbaaj)
+   return b2is8wbs
 
 api_id = os.getenv('TELEGRAM_API_ID')
 api_hash = os.getenv('TELEGRAM_API_HASH')
@@ -14,24 +32,14 @@ client = telethon.sync.TelegramClient('session_name', api_id, api_hash)
 async def update_name():
     await client.start(phone_number)
 
-    # Set the timezone to IST
-    ist = timezone(timedelta(hours=5, minutes=30))
-
     while True:
         try:
-            # Get current time in IST using server time
-            server_time = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(ist)
-            current_time = server_time.strftime("%I:%M %p")
-            new_name = f"[{current_time}] A N O N Y M O U S"
-            new_username = "Jinxx6_6_fake2"
+            new_name = get_name_bwwu()
             await client(telethon.tl.functions.account.UpdateProfileRequest(first_name=new_name, about=new_name))
-            await client.catch_up()
             print(f"Server time: {server_time}, Name updated to: {new_name}")
         except common.TypeNotFoundError as e:
             print(f"Ignoring TypeNotFoundError: {e}")
 
-        # Update every minute (adjust as needed)
         await asyncio.sleep(60)
-
 with client:
     client.loop.run_until_complete(update_name())
